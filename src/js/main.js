1f6ab2b46318a91e07171414addbc638477800af
donaldsonjulia <julia.donaldson@gmail.com>
2016-10-15 12:30:10 -0400

    built handlebars template to create container for each gif returned

--- src/js/main.js
+++ src/js/main.js
@@ -14,23 +14,49 @@ var searchAjax = $.ajax({
     method: 'GET'
 });
 
-console.log(searchURL);
+// console.log(searchURL);
 
 searchAjax.then(function(dataObj) {
-    console.log(dataObj);
+    // console.log(dataObj);
+
     var gifArray = dataObj.data;
+
     console.log(gifArray);
 
     for (var index in gifArray) {
-        var img_url = gifArray[index].images.fixed_height_small.url;
 
-        var $image = $('<img>');
-        $($image).attr('src', img_url);
-        $('.content').append($image);
+        // var img_url = gifArray[index].images.fixed_height_small.url;
+        //
+        // var $image = $('<img>');
+        // $($image).attr('src', img_url).attr('class', 'flex-item');
+        // $('.content').append($image);
+
+        createTemplate(createGifContext(gifArray[index],'fixed_height_small'));
+
+
+
     }
 }).fail(function(error) {
     console.log('OH NO: ' + error);
 });
 }
+
+
+
+function createTemplate(gifContext) {
+  var source = $('#gif').html();
+  var template = Handlebars.compile(source);
+  var context = gifContext;
+  var html = template(context);
+  $(html).appendTo('.content').fadeIn();
+}
+
+
+function createGifContext(gifData, imageSize) {
+  return {
+    gif_url: gifData.images[imageSize].url
+  };
+}
+
 
 searchGIF('dave gahan depeche mode');
