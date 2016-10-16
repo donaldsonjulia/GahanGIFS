8df6082c5afab0c6b60a489ce0b6b6475c9cc09c
donaldsonjulia <julia.donaldson@gmail.com>
2016-10-15 23:45:56 -0400

    updates to search and layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,34 @@
 // JULIA PROJECT
 
+var alreadySeen = 0;
 
 
+/**
+* Searches and connects to Giphy API for related gif images based on search terms
+* Initiates createGifContext and createTemplate functions
+* @function searchGIF
+* @param {string} queryString - search terms provided by user input
+* @param {number} alreadySeen - current count of search results that have already been displayed
+*/
 
-
-
-function searchGIF(queryString) {
-
-
+function searchGIF(queryString, alreadySeen) {
     var query = encodeURI(queryString);
+    var resultsLimit = 10;
 
-    var searchURL = 'http://api.giphy.com/v1/gifs/search?q=' + query + '&api_key=dc6zaTOxFJmzC';
+    var searchURL = 'http://api.giphy.com/v1/gifs/search?q=' + query + '&limit=' + resultsLimit + '&offset=' + alreadySeen + '&api_key=dc6zaTOxFJmzC';
 
     var searchAjax = $.ajax({
         url: searchURL,
         method: 'GET'
     });
 
-    // console.log(searchURL);
-
     searchAjax.then(function(dataObj) {
-        // console.log(dataObj);
+        console.log(dataObj);
 
         var gifArray = dataObj.data;
+        console.log(gifArray);
 
-        var imageSize = 'fixed_height_small';
+        var imageSize = 'fixed_height_downsampled';
 
         console.log(gifArray);
 
@@ -40,22 +44,22 @@ function searchGIF(queryString) {
 
 
 /**
-* Creates the context of each image to be passed to our html template
-* @function createGifContext
-* @param {object} gifData - the returned data for an individual gif from Giphy API
-* @param {string} imageSize - select image size in order to specify  correct image URL. gifData image object includes the following options (to see more extensive options, inspect object):
-* "downsized" (height and width depend on which image)
-* "downsized-large" (height and width depend on which image)
-* "downsized-medium" (height and width depend on which image)
-* "fixed_height" (height: 200px)
-* "fixed_height_downsampled" (height: 200px)
-* "fixed_height_small" (height: 100px)
-* "fixed_width" (width: 200px)
-* "fixed_width_downsampled" (width: 200px)
-* "fixed_width_small" (width: 100px)
-* "original" (height and width depend on which image)
-* @returns {object} gifContext
-*/
+ * Creates the context of each image to be passed to our html template
+ * @function createGifContext
+ * @param {object} gifData - the returned data for an individual gif from Giphy API
+ * @param {string} imageSize - select image size in order to specify  correct image URL. gifData image object includes the following options (to see more extensive options, inspect object):
+ * "downsized" (height and width depend on which image)
+ * "downsized-large" (height and width depend on which image)
+ * "downsized-medium" (height and width depend on which image)
+ * "fixed_height" (height: 200px)
+ * "fixed_height_downsampled" (height: 200px)
+ * "fixed_height_small" (height: 100px)
+ * "fixed_width" (width: 200px)
+ * "fixed_width_downsampled" (width: 200px)
+ * "fixed_width_small" (width: 100px)
+ * "original" (height and width depend on which image)
+ * @returns {object} gifContext
+ */
 
 function createGifContext(gifData, imageSize) {
     return {
@@ -65,37 +69,33 @@ function createGifContext(gifData, imageSize) {
 
 
 /**
-*Creates html elements for each image using Handlebars template
-*@function createTemplate
-*@param {object} gifContext - the context object for an individual gif that includes image URL
-*/
+ *Creates html elements for each image using Handlebars template
+ *@function createTemplate
+ *@param {object} gifContext - the context object for an individual gif that includes image URL
+ */
 function createTemplate(gifContext) {
     var source = $('#gif').html();
     var template = Handlebars.compile(source);
     var context = gifContext;
     var html = template(context);
-    $(html).prependTo('.content').fadeIn();
+    $(html).fadeIn('slow').prependTo('.content');
 }
 
 /**
-* Removes an individual image from content container when user clicks delete button
-*/
+ * Removes an individual image from content container when user clicks delete button
+ */
 $('.content').on('click', '.delete_button', function(event) {
-  $(this).parents('.gif-container').fadeOut(function() {
-    this.remove();
-  });
+    $(this).parents('.gif-container').fadeOut(function() {
+        this.remove();
+    });
 });
 
 /**
-* Initiates @function searchGIF upon search form submit
-*/
+ * Initiates @function searchGIF upon search form submit
+ */
 $('.search-form').on('click', '.search-button', function(event) {
-  event.preventDefault();
-  var searchString = $('.search-field').val();
-  console.log(searchString + " is what you searched");
-  searchGIF(searchString);
-  });
-
-
-
-searchGIF('dave gahan depeche mode');
+    event.preventDefault();
+    var searchString = $('.search-field').val();
+    $('.search-field').val('');
+    searchGIF(searchString, alreadySeen);
+});
